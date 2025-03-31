b834638aa7c8c6c7e8ab0064d6ad207d717373bb
Maxscout <maxscoutmine@gmail.com>
2025-03-30 20:44:41 -0400

    Add a license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+/*
+This file is part of cuRSed.
+
+cuRSed is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+cuRSed is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with cuRSed. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #![allow(non_snake_case)] // "cuRSed" is considered non snake case so just turn it off
 pub mod style;
 mod window;
